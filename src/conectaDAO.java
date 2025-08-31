ffa293d5573a2391f1c1ab2ccf8717b2de9e2fae
Antonio Murilo <antoniomurilog@gmail.com>
2025-08-31 13:10:20 -0300

    Primeira versão do sistema 1.0.0

---
+++ src/conectaDAO.java
@@ -0,0 +1,38 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Adm
+ */
+public class conectaDAO {
+    
+    // Campo de URL usuario e senha do banco de dados
+    String url = "jdbc:mysql://localhost/uc11"; //Aqui vai o caminho do seu banco de dados
+    String user = "root";  // Aqui vai o usuario do seu banco de dados
+    String password = "113322"; // aqui vai a senha do seu banco de dados
+    
+    public Connection connectDB(){
+        Connection conn = null;
+        
+        try {
+        
+            conn = DriverManager.getConnection(url, user, password);
+            
+        } catch (SQLException erro){
+            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+        }
+        return conn;
+    }
+    
+}
